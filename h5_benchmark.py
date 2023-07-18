3bab1ba57e5076360c945966f57b897bfe444c1f
Forrest Williams <ffwilliams2@alaska.edu>
2023-07-18 01:11:50 +0000

    working initial version

--- h5_benchmark.py
+++ h5_benchmark.py
@@ -17,11 +17,12 @@ import s3fs
 KB = 1024
 MB = KB * KB
 
-S3_FILESYSTEM = s3fs.S3FileSystem(anon=True)  # , default_block_size=8*MB
+S3_FILESYSTEM = s3fs.S3FileSystem(anon=True, default_block_size=24*MB)
 bucket_name = 'ffwilliams2-shenanigans'
 prefix = 'h5repack'
 # filename = '/Users/ffwilliams2/data/hdf5/Haywrd_14501_21043_012_210602_L090_CX_129_02.h5'
-filename = '/Users/ffwilliams2/data/hdf5/nisar_repaged.h5'
+# filename = '/Users/ffwilliams2/data/hdf5/nisar_repaged.h5'
+# filename = '/Users/ffwilliams2/data/hdf5/nisar_repaged.h5'
 base_s3_url = f's3://{bucket_name}/{prefix}/'
 
 
@@ -31,7 +32,7 @@ class GetRequestCounter(logging.Handler):
         self.count = 0
 
     def emit(self, record):
-        if record.msg.startswith('Sending GET request'):
+        if record.getMessage().startswith('CALL: get_object'):
             self.count += 1
 
 
@@ -52,11 +53,12 @@ def print_hdf5_information(file, dataset):
     print('Metadata Cache Min Size (MB):', mdc_min_nbytes / MB)
 
 
-def test_routine(fobj):
-    ds = h5py.File(fobj, 'r')
+def test_routine(fobj, extra_args):
+    ds = h5py.File(fobj, mode='r', **extra_args)
     vv = ds['science']['LSAR']['SLC']['swaths']['frequencyA']['VV']
-    # data = vv[3000:8000, 500:2000]
-    data = vv[:, :]
+    # data = vv[0:20, 0:20]
+    data = vv[3000:8000, 500:2000]
+    # data = vv[:, :]
 
 
 def benchmark(filepath, name, local=True):
@@ -64,24 +66,31 @@ def benchmark(filepath, name, local=True):
     counter_handler = GetRequestCounter()
     logger.addHandler(counter_handler)
     logger.setLevel(logging.DEBUG)
+    extra_args = {}
+    if 'repaged' in filepath:
+        extra_args['page_buf_size'] = 8*MB
+
+    if 'rechunkeed' in filepath:
+        extra_args['rdcc_bytes'] = 64*MB
+
     start_time = time.time()
     if local:
         with open(filepath, 'rb') as fobj:
-            test_routine(fobj)
+            test_routine(fobj, extra_args)
     else:
         with S3_FILESYSTEM.open(filepath, mode='rb') as fobj:
-            test_routine(fobj)
+            test_routine(fobj, extra_args)
     stop_time = time.time()
     print(f'{name} time: {stop_time-start_time:.4f}')
     print(f'Number of GET requests: {counter_handler.count}')
 
 
 def run_benchmarks():
-    benchmark(filename, 'Unaltered Local', local=True)
-    # benchmark(base_s3_url + filename, 'unaltered s3', local=false)
-    # benchmark(base_s3_url + 'nisar_repaged.h5', 'repacked metadata s3', local=false)
+    benchmark('Haywrd_14501_21043_012_210602_L090_CX_129_02.h5', 'Unaltered Local', local=True)
+    benchmark(base_s3_url + 'Haywrd_14501_21043_012_210602_L090_CX_129_02.h5', 'unaltered s3', local=False)
+    # benchmark(base_s3_url + 'nisar_repaged_.h5', 'repacked metadata s3', local=False)
     # benchmark(base_s3_url + 'nisar_rechunked.h5', 'rechunked data s3', local=false)
-    # benchmark(base_s3_url + 'nisar_repaged_rechunked.h5', 'repacked metadata and rechunked data', local=false)
+    benchmark(base_s3_url + 'nisar_repaged_rechunked.h5', 'repacked metadata and rechunked data', local=False)
 
 
 def run_benchmark():
